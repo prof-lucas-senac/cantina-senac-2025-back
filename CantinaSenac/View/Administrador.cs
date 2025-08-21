09827cd8ba5c329f8beacaf599a857b955b42867
André <andretangerina03@gmail.com>
2025-08-20 22:01:10 -0300

    Primeiro commit

---
+++ CantinaSenac/View/Administrador.cs
@@ -0,0 +1,60 @@
+public class Administrador : Usuario
+{
+    public void Suspender()
+    {
+
+    }
+
+    public void Deletar()
+    {
+
+    }
+
+    public void ListarTodos()
+    {
+
+    }
+
+    public void RecuperarHistorico()
+    {
+
+    }
+
+    public void CadastrarCurso()
+    {
+
+    }
+
+    public void AtualizarCurso()
+    {
+
+    }
+
+    public void DeletarCurso()
+    {
+
+    }
+    
+    public override void Cadastrar()
+    {
+        
+    }
+
+
+    public override void Atualizar()
+    {
+        
+    }
+
+    public override  void PublicarFeedback() {}
+
+    public override void ComentarFeedback()
+    {
+        
+    }
+
+    public override void Logar()
+    {
+        
+    }
+}
---
+++ CantinaSenac/View/Aluno.cs
@@ -0,0 +1,28 @@
+public class Aluno : Usuario
+{
+    public Curso curso { get; set; }
+
+    public override void Cadastrar()
+    {
+        
+    }
+
+
+    public override void Atualizar()
+    {
+        
+    }
+
+    public override  void PublicarFeedback() {}
+
+    public override void ComentarFeedback()
+    {
+        
+    }
+
+    public override void Logar()
+    {
+        
+    }
+
+}
