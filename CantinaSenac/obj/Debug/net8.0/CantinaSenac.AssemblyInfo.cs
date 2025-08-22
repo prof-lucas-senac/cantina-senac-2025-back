48b2b01c5fc424f73cfa2b69e8aa436bb42e2b66
Guilherme <guilherme.macedo.varela26@gmail.com>
2025-08-22 19:23:36 -0300

    trabalho 1

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba54defaccedf33210e332cab765b47f336c2f53")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
