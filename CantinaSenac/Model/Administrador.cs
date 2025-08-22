6a10ec8fa0c3159e74a23021dbe0e19a935d57e1
jvictormelo <jvmelo0x@gmail.com>
2025-08-22 19:40:46 -0300

    att teste

--- CantinaSenac/Model/Administrador.cs
+++ CantinaSenac/Model/Administrador.cs
@@ -49,4 +49,6 @@ public class Admnistrador : Usuario
 
     }
 
+
+
 }
---
+++ CantinaSenac/Model/historicoFeedback.cs
@@ -0,0 +1,8 @@
+public class historicoFeedback
+{
+    public DateTime dataPublicacao { get; set; }
+    public Postagem postagem { get; set; }
+    public Usuario usuario { get; set; }
+
+    
+}
