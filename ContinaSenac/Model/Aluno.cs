49665cff45596f39555a388d5b407dd54ed8d8a9
Isa Padilha <isap4619@gmail.com>
2025-08-27 21:33:02 -0300

    Pagina de Login e Senha

--- ContinaSenac/Model/Aluno.cs
+++ ContinaSenac/Model/Aluno.cs
@@ -1,5 +1,27 @@
-public abstract class Aluno : Usuario
-{ 
+public class Aluno : Usuario
+{
+    public override void Atualizar()
+    {
+        throw new NotImplementedException();
+    }
 
+    public override void Cadastrar()
+    {
+        throw new NotImplementedException();
+    }
 
+    public override void ComentarFeedBack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Logar()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void PublicarFeedBack()
+    {
+        throw new NotImplementedException();
+    }
 }
