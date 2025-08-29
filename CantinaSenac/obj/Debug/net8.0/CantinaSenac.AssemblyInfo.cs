48132c9dea2a2cf8a0ec55b6939e6fc56ed1eef6
Samuel Hansen <samuelhansen07@hotmail.com>
2025-08-29 19:45:22 -0300

    Alteração nas entidades

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dcb6b9b3f72949c2589d20a9e69f3b6668c120b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207c115be30607c6aee185eea45975ee27d4cec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
