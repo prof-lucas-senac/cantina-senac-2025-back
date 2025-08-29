186940aac92f21f37f149a61740ae57eb9e59999
jvictormelo <jvmelo0x@gmail.com>
2025-08-29 20:26:20 -0300

    Att merge

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12562688ee34fd5bfeb79eea8b27aa700dde5526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e519ec5b779ff77928cbfb0c677a4fe038f9313")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
