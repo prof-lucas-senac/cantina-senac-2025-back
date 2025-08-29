e0a447d1887398b2d2dd5ec6bc00fa480b6ca9f9
André <andretangerina03@gmail.com>
2025-08-29 20:33:56 -0300

    Current merging

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12562688ee34fd5bfeb79eea8b27aa700dde5526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f6f0c72633e00f5dc529f93d6d9480c0268338")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
