b9eb652a0b54553eed47d03ae934f6ea9d295087
Marcos <marcosantonio240604@gmail.com>
2025-08-29 21:04:35 -0300

    merge

--- CantinaSenac/Model/Aluno.cs
+++ CantinaSenac/Model/Aluno.cs
@@ -1,50 +1,24 @@
-<<<<<<< HEAD
-using Microsoft.Win32.SafeHandles;
+
 
 public class Aluno : Usuario
 {
     public Curso curso{ get; set; }
     public override void Atualizar()
     {
-=======
-class Aluno : Usuario
-{
-    // public List<Curso> Cursos { get; set; } = new List<Curso>();
-    public override void Atualizar()
-    {
-        throw new NotImplementedException();
->>>>>>> master
     }
-
     public override void Cadastrar()
     {
-<<<<<<< HEAD
-=======
-        throw new NotImplementedException();
->>>>>>> master
     }
 
     public override void ComentarFeedback()
     {
-<<<<<<< HEAD
-=======
-        throw new NotImplementedException();
->>>>>>> master
     }
 
     public override void Logar()
     {
-<<<<<<< HEAD
-=======
-        throw new NotImplementedException();
->>>>>>> master
     }
 
     public override void PublicarFeedback()
     {
-<<<<<<< HEAD
-=======
-        throw new NotImplementedException();
->>>>>>> master
     }
 }
--- CantinaSenac/Model/Usuario.cs
+++ CantinaSenac/Model/Usuario.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 public abstract class Usuario
-=======
-abstract class Usuario
->>>>>>> master
 {
     public int Id { get; set; }
     public string NomeDoUsuario { get; set; }
@@ -10,19 +6,11 @@ abstract class Usuario
     public string Senha { get; set; }
     public int Status { get; set; }
     public string Foto { get; set; }
-<<<<<<< HEAD
     public List<Postagem> Postagens { get; set; }
-=======
-    // public List<Postagem> Postagens { get; set; }
->>>>>>> master
 
     public abstract void Cadastrar();
     public abstract void Atualizar();
     public abstract void PublicarFeedback();
     public abstract void ComentarFeedback();
     public abstract void Logar();
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 }
