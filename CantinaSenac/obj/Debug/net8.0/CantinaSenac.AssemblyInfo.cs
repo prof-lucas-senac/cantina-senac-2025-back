86441837d32b6f854706a9961ecceac56f301220
Alan Oliveira <alarespawnables08@gmail.com>
2025-09-03 19:04:11 -0300

    arquivo alterado

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c026c77a498bcf50d3144e5fc1740457c9e53c1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a902a5ebbd69782953ddcdc601d5cf41291cdd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
