34f1f8fa12de0e4e4c211a1c691b4d9fd80acf43
Samuel Hansen <samuelhansen07@hotmail.com>
2025-09-10 19:38:37 -0300

    atualização da master

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eebcae29cd4fe7b1a6942d414f517d3e6afef998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba70bf8bbe58da4ff0c3d3f73913d90b33f1718")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
