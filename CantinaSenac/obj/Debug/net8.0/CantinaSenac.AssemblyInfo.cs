8e031d731906ccc13ac3005243171690c15b746e
Alan Oliveira <alarespawnables08@gmail.com>
2025-09-10 21:57:25 -0300

    att

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f810226464eeef8d4e2308ec1b107f929e8d4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f89d69caf3328f1d217d4ab586c3fb4e5bcc1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
