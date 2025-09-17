2f346c99053233dbeccafe4118568a1f75397aea
RicardoMachadoOFC <cado.machado@hotmail.com>
2025-09-17 19:45:43 -0300

    exclusão de repo

--- Cantina-senac/DAL/IDAO.cs
+++
@@ -1,8 +0,0 @@
-interface IDAO <T>
-{
-    public void Cadastrar(T objeto);
-    public void Atualizar(T objeto);
-    public void Excluir(int id);
-    public List<T> ListarTodos();
-    public T Listar(int id);
-}
