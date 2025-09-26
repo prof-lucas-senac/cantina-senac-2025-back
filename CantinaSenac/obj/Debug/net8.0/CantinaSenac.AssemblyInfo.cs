58c3fc2a3ae141ab7990b6efc7693469536b5d51
Prog_Sis <lucas.barros@prof.sc.senac.br>
2025-09-26 20:43:46 -0300

    Aula 12: Implementação de funcionalidade de atualização de Feedbacks

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6772b5bcdc4362acc5c63648ae97f6c6a315b9dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e2b0a810dcca533c802f9d55548493f576917f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
