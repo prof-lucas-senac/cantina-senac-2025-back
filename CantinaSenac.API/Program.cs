a3fef6757dfdd6277157bd01d9c0950f20923d29
unknown <Prog_Sis@LGE202D056053.alunos.sc.senac.br>
2025-10-10 21:48:51 -0300

    implementação da API

---
+++ CantinaSenac.API/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/feedbacks", () =>
+{
+    List<Feedback> feedbacks;
+    feedbacks = new FeedbackController().ListarFeedbacks();
+    return feedbacks;
+});
+
+app.Run();
