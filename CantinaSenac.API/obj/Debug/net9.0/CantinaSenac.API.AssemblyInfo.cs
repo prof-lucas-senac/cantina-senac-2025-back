ec6b1c5e134164cf863d6c376513f88518cb2381
RicardoMachadoOFC <cado.machado@hotmail.com>
2025-10-16 19:11:03 -0300

    att

--- CantinaSenac.API/obj/Debug/net9.0/CantinaSenac.API.AssemblyInfo.cs
+++ CantinaSenac.API/obj/Debug/net9.0/CantinaSenac.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee7859b206cff3f2e0faff98432819968cf385b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2be53f558153ceed109c976f35df416c68d5ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
