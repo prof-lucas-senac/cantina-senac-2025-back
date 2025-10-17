0d6938a68e92e410c2943adddb537d0a70158be3
Alan Oliveira <alarespawnables08@gmail.com>
2025-10-16 21:50:51 -0300

    feat(api): integração de endpoints com Postman no VSCode e ajustes em controllers

--- CantinaSenac.API/Program.cs
+++ CantinaSenac.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -12,5 +14,24 @@ app.MapGet("/feedbacks", () =>
     feedbacks = new FeedbackController().ListarFeedbacks();
     return feedbacks;
 });
+
+app.MapPost("/feedbacks", ([FromBody] Feedback feedback) =>
+{
+    new FeedbackController().PostarFeedback(feedback);
+    return "Feedback adicionado com sucesso! ";
+});
+
+app.MapDelete("/feedbacks", ([FromBody] Feedback feedback) =>
+{
+    new FeedbackController().ExcluirFeedback(feedback);
+    return "Feedback excluido com sucesso! ";
+});
+
+app.MapPut("/feedbacks", ([FromBody] Feedback feedback) =>
+{
+    new FeedbackController().AtualizarFeedback(feedback);
+    return "Feedback atualizado com sucesso! ";
+});
+
 
 app.Run();
