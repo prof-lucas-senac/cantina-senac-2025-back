c6576548509b017731a9e75f93611514279275d9
jvictormelo <jvmelo0x@gmail.com>
2025-10-16 21:55:52 -0300

    att cantinaAPI com metodos PUT E POST

--- CantinaSenac.API/Program.cs
+++ CantinaSenac.API/Program.cs
@@ -1,13 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "olá bb");
-app.MapGet("/feedbacks",() =>
+app.MapGet("/motivacional", () => "Tem boleto pra pagar.");
+app.MapGet("/feedbacks", () =>
 {
     List<Feedback> feedbacks;
     feedbacks = new FeedbackController().Listar();
-    return Results.Json(feedbacks);
+    return feedbacks;
 }
 );
+app.MapPost("/feedbacks", ([FromBody] Feedback feedback) =>
+{
+    new FeedbackController().PostarFeedback(feedback);
+    return "Feedback cadastrado com sucesso!";
+});
+
+app.MapPut("/feedbacks", ([FromBody] Feedback feedback) =>
+{
+    new FeedbackController().AtualizarFeedback(feedback, feedback.Descricao);
+    return "Feedback atualizado com sucesso!";
+});
 
 app.Run();
