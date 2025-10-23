c5068a3fb8e3946c928646f20331d0f375021b64
Prog_Sis <lucas.barros@prof.sc.senac.br>
2025-10-22 21:59:16 -0300

    Aula 18: implementação de DELETE e códigos de status.

--- CantinaSenac.API/Program.cs
+++ CantinaSenac.API/Program.cs
@@ -12,19 +12,26 @@ app.MapGet("/feedbacks", () =>
 {
     List<Feedback> feedbacks;
     feedbacks = new FeedbackController().ListarFeedbacks();
-    return feedbacks;
+    return Results.Ok(feedbacks);
 });
 
 app.MapPost("/feedbacks", ([FromBody] Feedback feedback) =>
 {
     new FeedbackController().PostarFeedback(feedback);
-    return "Feedback adicionado com sucesso.";
+    return Results.Ok("Feedback criado com sucesso.");
 });
 
 app.MapPut("/feedbacks", ([FromBody] Feedback feedback) =>
 {
     new FeedbackController().AtualizarFeedback(feedback);
     return "Feedback atualizado com sucesso.";
+});
+
+app.MapDelete("/feedbacks", ([FromBody] Feedback feedback) =>
+{
+    
+    new FeedbackController().ExcluirFeedback(feedback);
+    return Results.Ok("Feedback excluído com sucesso.");
 });
 
 app.Run();
