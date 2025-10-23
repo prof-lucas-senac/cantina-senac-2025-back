bc0a68f4daa5a75b8f7e84e8b2db26c1716398f8
André <andretangerina03@gmail.com>
2025-10-22 21:59:00 -0300

    Implementação de mensagens http e adição do método delete

--- CantinaSenac.API/Program.cs
+++ CantinaSenac.API/Program.cs
@@ -12,21 +12,36 @@ app.MapGet("/feedbacks", () =>
 {
     List<Feedback> feedbacks;
     feedbacks = new FeedbackController().ListarFeedbacks();
-    return feedbacks;
+    return Results.Ok(feedbacks);
 });
 
 app.MapPost("/feedbacks", ([FromBody] Feedback feedback) =>
 {
 
     new FeedbackController().PostarFeedback(feedback);
-    return "Feedback adicionado com sucesso";
+    return Results.Ok("Feedback criado com sucesso");
 
 });
 
 app.MapPut("/feedbacks", ([FromBody] Feedback feedback) =>
 {
     new FeedbackController().AtualizarFeedback(feedback);
-    return "Feedback atualizado com sucesso";
+     return Results.Ok("Feedback alterado com sucesso");
+});
+
+app.MapDelete("/feedbacks", ([FromBody] Feedback feedback) =>
+{
+     if(feedback.UsuarioId == 1)
+    {
+         new FeedbackController().ExcluirFeedback(feedback);
+
+    return Results.Ok("Feedback excluído com sucesso");
+    }
+   else
+    {
+        return Results.Forbid();
+    }
+
 });
 
 app.Run();
