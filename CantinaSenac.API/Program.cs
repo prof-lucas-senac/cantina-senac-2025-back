cff301026ccba8a5952b3f073f65a0345407cbf3
Prog_Sis <lucas.barros@prof.sc.senac.br>
2025-10-24 21:56:21 -0300

    Aula 19: agrupamento de endpoints

--- CantinaSenac.API/Program.cs
+++ CantinaSenac.API/Program.cs
@@ -3,35 +3,56 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var feedbackGroup = app.MapGroup("/feedbacks");
 
-// Endpoint que retorne uma frase motivacional
-app.MapGet("/motivacional", () => "Tem boleto pra pagar.");
-
-app.MapGet("/feedbacks", () =>
+feedbackGroup.MapGet("", () =>
 {
     List<Feedback> feedbacks;
     feedbacks = new FeedbackController().ListarFeedbacks();
     return Results.Ok(feedbacks);
 });
 
-app.MapPost("/feedbacks", ([FromBody] Feedback feedback) =>
+feedbackGroup.MapPost("", ([FromBody] Feedback feedback) =>
 {
     new FeedbackController().PostarFeedback(feedback);
     return Results.Ok("Feedback criado com sucesso.");
 });
 
-app.MapPut("/feedbacks", ([FromBody] Feedback feedback) =>
+feedbackGroup.MapPut("", ([FromBody] Feedback feedback) =>
 {
     new FeedbackController().AtualizarFeedback(feedback);
     return "Feedback atualizado com sucesso.";
 });
 
-app.MapDelete("/feedbacks", ([FromBody] Feedback feedback) =>
+feedbackGroup.MapDelete("", ([FromBody] Feedback feedback) =>
 {
-    
     new FeedbackController().ExcluirFeedback(feedback);
     return Results.Ok("Feedback excluído com sucesso.");
+});
+
+app.MapGet("/alunos", () =>
+{
+    List<Aluno> alunos;
+    alunos = new AlunoController().ListarAlunos();
+    return Results.Ok(alunos);
+});
+
+app.MapPost("/alunos", ([FromBody] Aluno aluno) =>
+{
+    new AlunoController().Adicionar(aluno);
+    return Results.Ok("Aluno criado com sucesso.");
+});
+
+app.MapPut("/alunos", ([FromBody] Aluno aluno) =>
+{
+    new AlunoController().AtualizarAluno(aluno);
+    return "Aluno atualizado com sucesso.";
+});
+
+app.MapDelete("/alunos", ([FromBody] Aluno aluno) =>
+{
+    new AlunoController().ExcluirAluno(aluno);
+    return Results.Ok("Aluno excluído com sucesso.");
 });
 
 app.Run();
