9365e06a43f02c32c7c7418796f80a6b08d2baca
Alexander Filho <alexanderaraldi@gmail.com>
2025-11-04 19:50:21 -0300

    Merge from master

--- CantinaSenac.API/obj/Debug/net8.0/CantinaSenac.API.AssemblyInfo.cs
+++ CantinaSenac.API/obj/Debug/net8.0/CantinaSenac.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff301026ccba8a5952b3f073f65a0345407cbf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b28abf2f415a2379198c8778cd9804016d5bcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
