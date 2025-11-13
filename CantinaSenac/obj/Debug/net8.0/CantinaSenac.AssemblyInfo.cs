3c3c78a82a55d63cfb06fbc7e773b28c2b6a7cfe
unknown <Prog_Sis@LGE202D056043.alunos.sc.senac.br>
2025-11-12 21:47:10 -0300

    publicando o back na web com swagger

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4bf429add8ead2f437d6118ed69326bb703297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30dc226dd35da0eb3399cce1594c40142293e3c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
