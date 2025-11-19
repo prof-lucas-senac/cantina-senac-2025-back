393a82327c6fc98508b961718cd4dff4f38561d2
unknown <Prog_Sis@LGE202D056043.alunos.sc.senac.br>
2025-11-18 21:17:10 -0300

    Atualização

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30dc226dd35da0eb3399cce1594c40142293e3c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3c78a82a55d63cfb06fbc7e773b28c2b6a7cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
